af73a00cad2f42424aecc08f8f4d471003e56fed
Stefano-Para <stefano.zina@gmail.com>
2021-07-08 17:01:03 +0200

    costructor + caso senza dati (costructor)

--- index.php
+++ index.php
@@ -10,53 +10,76 @@ class Movie {
     public $director;
     public $rating;
 
-    public function setRating($voto) {
-        if ($voto <= 3.0) {
+
+    function __construct($title = "Title unknown", $director = "Director unknown", $rating = 0) {
+        $this->title = $title;
+        $this->director =  $director;
+        $this->rating = $rating;
+    }
+    // public function setRating($newRating) {
+    //     $this->rating = $newRating;
+    // }
+
+    public function getRating() {
+        $voto = $this->rating;
+        if ($voto <= 3.0 && $voto >0) {
             return "<h2 style=\"color: brown;\">" . "This is not the best film you'll ever seen.." . " " . "(Voto: $voto/5)" . "</h2>";
         } elseif($voto < 4.0 && $voto > 3.0) {
             return "<h2 style=\"color: orange;\">" . "This is an average film" . " " . "(Voto: $voto/5)" . "</h2>";
-        } else {
+        } elseif ($voto > 5.0) {
             return "<h2 style=\"color: red;\">" . "This is a GREAT film!" . " " . "(Voto: $voto/5)" . "</h2>";
+        } elseif ($voto == 0) {
+            return "<h2 style=\"color: blue;\">" . "Can't find any votes about this film" . "</h2>";
         }
     }
 }
 
-// 1/3
-$film1 = new Movie();
-$film1->title = "Trainspotting";
-$film1->director = "Danny Boyle";
-$film1->rating = 3.9;
+// 1/4
+$film1 = new Movie("Trainspotting", "Danny Boyle", 3.9);
+// $film1 = new Movie();
+// $film1->title = "Trainspotting";
+// $film1->director = "Danny Boyle";
+// $film1->rating = 3.9;
+
 // var_dump($film1);
 echo "<h2>" . $film1->title . "</h2>"
     . "<h3>" . $film1->director . "</h3>"
-    . "<h5>" . $film1->setRating($film1->rating) . "</h5>"
+    . "<h5>" . $film1->getRating() . "</h5>"
     . "<hr>";
 
-// 2/3
-$film2 = new Movie();
-$film2->title = "Il grande Lebowski";
-$film2->director = "Fratelli Coen";
-$film2->rating = 4.4;
+// 2/4
+$film2 = new Movie("Il grande Lebowski", "Fratelli Coen", 4.4);
+// $film2 = new Movie();
+// $film2->title = "Il grande Lebowski";
+// $film2->director = "Fratelli Coen";
+// $film2->rating = 4.4;
 // var_dump($film2);
 echo "<h2>" . $film2->title . "</h2>"
     . "<h3>" . $film2->director . "</h3>"
-    . "<h5>" . $film2->setRating($film2->rating) . "</h5>"
+    . "<h5>" . $film2->getRating() . "</h5>"
     . "<hr>";
 
-
-// 3/3
-$film3 = new Movie();
-$film3->title = "The Disaster Artist";
-$film3->director = "James Franco";
-$film3->rating = 3;
+// 3/4
+$film3 = new Movie("The Disaster Artist", "James Franco", 3);
+// $film3 = new Movie();
+// $film3->title = "The Disaster Artist";
+// $film3->director = "James Franco";
+// $film3->rating = 3;
 // var_dump($film3);
 echo "<h2>" . $film3->title . "</h2>"
     . "<h3>" . $film3->director . "</h3>"
-    . "<h5>" . $film3->setRating($film3->rating) . "</h5>"
+    . "<h5>" . $film3->getRating() . "</h5>"
+    . "<hr>";
+
+// 4/4
+$film4 = new Movie();
+echo "<h2>" . $film4->title . "</h2>"
+    . "<h3>" . $film4->director . "</h3>"
+    . "<h5>" . $film4->getRating() . "</h5>"
     . "<hr>";
 
 
-
+//copyrightssssss
 echo "<h6>" . "!! I N C R E D I B L E !! layout <br> from PARA" . "</h6>"
 
 ?>
